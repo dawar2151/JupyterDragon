3202b76ce7a509b2ddf0bd8b249d41c480b49a43
TinySmartMaster <zouhair.et.taousy@gmail.com>
2017-11-12 17:22:54 +0100

    get current cell contents, add jupiter button

--- extension/jupyterdragon.js
+++ extension/jupyterdragon.js
@@ -1,10 +1,26 @@
-/*
- Add this file to $(ipython locate)/nbextensions/jupyterDragon.js
- And load it with:
-*/ 
- require(["nbextensions/jupyterDragon"], function (jupyterDragon_extension) {
- 	console.log('jupyterDragon extension loaded');
- 	alert("GetStared");
- 	jupyterDragon_extension.load_ipython_extension();
- });
- 
+define([
+    'base/js/namespace' ], function(Jupyter) {
+    function load_ipython_extension() {
+        var handler = function () {
+           /* get current cell contents */
+            var cell = Jupyter.notebook.get_cell(Jupyter.notebook.get_selected_cells_indices());
+            console.log(cell);
+        };
+        console.log('hello world from jupyterDragon');
+        var action = {
+            icon: 'fa-comment-o', // a font-awesome class used on buttons, etc
+            help    : 'Show an alert',
+            help_index : 'zz',
+            handler : handler
+        };
+        var prefix = 'jupyterDragon';
+        var action_name = 'show-alert-usr';
+
+        var full_action_name = Jupyter.actions.register(action, action_name, prefix); // returns 'my_extension:show-alert'
+        Jupyter.toolbar.add_buttons_group([full_action_name]);
+    }
+
+    return {
+        load_ipython_extension: load_ipython_extension
+    }; 
+});
